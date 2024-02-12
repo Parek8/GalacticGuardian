f2a36753ed18a68eae028573445e6fdcdf073ffc
Párek8 <polakpetr8878@gmail.com>
2024-02-12 16:42:27 +0100

    Feat: Endless BG

--- Assets/Scripts/Background/EndlessBackground.cs
+++ Assets/Scripts/Background/EndlessBackground.cs
@@ -1,6 +1,10 @@
+using System.Collections.Generic;
 using UnityEngine;
 internal class EndlessBackground : MonoBehaviour
 {
+    [field: SerializeField] Transform BackgroundParent;
+
+    List<Transform> _backgrounds = new();
     Camera _mainCamera;
     Vector3 _screenBounds;
     Transform _player;
@@ -9,13 +13,24 @@ internal class EndlessBackground : MonoBehaviour
         _mainCamera = Camera.main;
         _screenBounds = _mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, _mainCamera.transform.position.z));
         _player = GameManager.GameManagerInstance.PlayerTransform;
+        Transform[] _bgs = BackgroundParent.GetComponentsInChildren<Transform>();
+        for (int i = 1; i < _bgs.Length; i++)
+            if (!_backgrounds.Contains(_bgs[i]))
+                _backgrounds.Add(_bgs[i]);
     }
 
     private void FixedUpdate()
     {
-        if (Vector3.Distance(_player.position, transform.position) >= 450)
-            gameObject.SetActive(false);
-        else
-            gameObject.SetActive(true);
+        foreach (Transform _bg in _backgrounds)
+        {
+            if (_bg.position.x < _player.position.x - 9)
+                _bg.position = new Vector3(_bg.position.x - 27, _bg.position.y, _bg.position.z);
+            if (_bg.position.x > _player.position.x - 9)
+                _bg.position = new Vector3(_bg.position.x + 27, _bg.position.y, _bg.position.z);
+            if (_bg.position.y < _player.position.y + 9)
+                _bg.position = new Vector3(_bg.position.x, _bg.position.y + 27, _bg.position.z);
+            if (_bg.position.y > _player.position.y + 9)
+                _bg.position = new Vector3(_bg.position.x, _bg.position.y - 27, _bg.position.z);
+        }
     }
 }
