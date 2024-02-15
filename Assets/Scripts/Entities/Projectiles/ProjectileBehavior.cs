02906ca52bbc8f4544086bd918ffdade5d6e6452
Parek8 <polakpetr8878@gmail.com>
2024-02-15 12:25:05 +0100

    Feat: Bullet Collision

--- Assets/Scripts/Entities/Projectiles/ProjectileBehavior.cs
+++ Assets/Scripts/Entities/Projectiles/ProjectileBehavior.cs
@@ -8,17 +8,18 @@ public class ProjectileBehavior : MonoBehaviour
 {
     [field: SerializeField] internal float ShootSpeed { get; private set;} = 4f;
     [SerializeField] private float DMG = 1f;
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
     public void SetDMG(float dmg)
     {
        DMG = dmg;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) 
+    {
+        EntityStats _stats;
+        if (other.gameObject.TryGetComponent<EntityStats>(out _stats))
+        {
+            _stats.TakeDamage(DMG);
+            Destroy(gameObject);
+        }
     }
 }
