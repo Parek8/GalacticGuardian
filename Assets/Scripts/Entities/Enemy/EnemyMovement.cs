4a2355d1176a238b4967e8307dd1a582ee720922
springik <springik.games@email.cz>
2024-02-17 19:09:00 +0100

    FIX/POLISH:Enemy shooting precision
    Enemy now shoots, when the player is actually in front of them, and no when they see the player

--- Assets/Scripts/Entities/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Entities/Enemy/EnemyMovement.cs
@@ -1,30 +1,39 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using UnityEngine;
 
+[RequireComponent(typeof(EntityStats))]
 public class EnemyMovement : MonoBehaviour
 {
-    [field: SerializeField] float _visibleDistance = 25;
+    [Tooltip("Transform representing the target.\nDefaults to the players' transform.")]
+    [field: SerializeField] Transform _pTransform;
     internal bool Agro => _agro;
-    Transform _pTransform;
     Vector2 _dir = Vector2.zero;
-    Vector2 _lookAxis = Vector2.up;
     EntityStats _stats;
     float _dot;
     bool _agro = false;
+    internal float Dot
+    {
+        get { return _dot; }
+        private set { _dot = value; }
+    }
+    [Tooltip("Represents the distance the enemy can see the target.")]
+    [field: SerializeField] float _visibleDistance = 25;
 
     // Start is called before the first frame update
     void Start()
     {
-        _pTransform = GameManager.GameManagerInstance.PlayerTransform;
+        if(_pTransform == null)
+            _pTransform = GameManager.GameManagerInstance.PlayerTransform;
+
         _stats = gameObject.GetComponent<EntityStats>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.DrawRay(transform.position, transform.up * _visibleDistance, Color.green);
         RotateToTarget();
         MoveToTarget();
     }
@@ -45,7 +54,6 @@ public class EnemyMovement : MonoBehaviour
         if(_dot > 0.38f && _dot <= 1 && _dir.magnitude < _visibleDistance)
         {
             transform.Rotate(0, 0, angle * cw * _stats.RotationSpeed);
-            //transform.rotation = Quaternion.Euler(Vector3.RotateTowards(transform.up, _dir, angle * cw, _stats.RotationSpeed));
         }
     }
 
@@ -53,11 +61,8 @@ public class EnemyMovement : MonoBehaviour
     {
         if (_dot > 0.38f && Vector2.Distance(transform.position, _pTransform.position) < _visibleDistance && Vector2.Distance(transform.position, _pTransform.position) > 1f)
         {
-            _agro = true;
+            _agro = _dot >= 0.91;
             transform.position = Vector2.MoveTowards(transform.position, _pTransform.position, Time.deltaTime * _stats.MovementSpeed * Time.deltaTime);
         }
-        else
-            _agro = false;
-
     }
 }
