d47bf1be3225dbbbbb1d2678e5cc0754dd206a6f
Párek8 <petrpolak11@zak.spsul.cz>
2024-03-10 21:40:42 +0100

    Feat: Enemy Distance From Player, Fix: Agro

--- Assets/Scripts/Entities/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Entities/Enemy/EnemyMovement.cs
@@ -8,7 +8,6 @@ public class EnemyMovement : MonoBehaviour
 {
     [Tooltip("Transform representing the target.\nDefaults to the players' transform.")]
     [field: SerializeField] Transform _pTransform;
-    [field: SerializeField] float _agroDistance;
     internal bool Agro => _agro;
     Vector2 _dir = Vector2.zero;
     EntityStats _stats;
@@ -69,13 +68,10 @@ public class EnemyMovement : MonoBehaviour
     {
         if (_pTransform != null)
         {
+            _agro = (_dot >= 0.91 && Vector2.Distance(transform.position, _pTransform.position) < _visibleDistance);
+            Debug.Log($"{transform.name} {Vector2.Distance(transform.position, _pTransform.position)}");
             if (/*_dot > 0.38f && Vector2.Distance(transform.position, _pTransform.position) < _visibleDistance && */ Vector2.Distance(transform.position, _pTransform.position) > 4f)
-            {
-                _agro = (_dot >= 0.91 && Vector2.Distance(transform.position, _pTransform.position) < _visibleDistance);
                 transform.position = Vector2.MoveTowards(transform.position, _pTransform.position, _stats.MovementSpeed * Time.deltaTime);
-            }
-            else
-                _agro = false;
         }
     }
 }
